79003e53f08cc79ca65e21951d2f94e2940b0403
mehul <gadhadaramehul786@gmail.com>
2025-06-01 17:35:56 +0530

    chatbackend

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const { ChatUser, Message } = require("./modu");
 
 const io = new Server(server, {
   cors: {
-    origin: [ "*"],
+    origin: "https://mchatapp.vercel.app",
     methods: ["GET", "POST"]
   }
 });
